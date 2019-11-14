8302f238551dc54f29843b0609b223714249ce37
SvenAlHamad <sven@webiny.com>
2019-11-14 16:07:51 +0000

    Updated plugin instructions

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,34 +1,32 @@
 module.exports = {
-    "docs": {
-        "Webiny": [
-            "webiny/introduction"
-        ],
-        "Get Started": [
-            "get-started/quick-start",
-            "get-started/going-live",
-            "get-started/development-principles"
-        ],
-        "Deep Dive": [
-            "deep-dive/project-structure",
-            "deep-dive/cloud-architecture"
-        ],
-        "API Development": [
-            "coming-soon",
-            /*"api-overview",
+  docs: {
+    Webiny: ['webiny/introduction'],
+    'Get Started': [
+      'get-started/quick-start',
+      'get-started/going-live',
+      'get-started/development-principles',
+    ],
+    'Deep Dive': [
+      'deep-dive/project-structure',
+      'deep-dive/cloud-architecture',
+    ],
+    'API Development': [
+      'coming-soon',
+      /*"api-overview",
              "api-schema-plugin",
              "api-schema-test",*/
     ],
     'App Development': ['coming-soon'],
     Plugins: [
-      'plugins/introduction',
+      'plugins/adding-your-plugin',
       // add your plugin page below, make sure it's ordered alphabetically
     ],
     Other: [
-        {
-            type: 'category',
-            label: 'FAQ',
-            items: ['other/faq/how-is-webiny-serverless'],
-        },
+      {
+        type: 'category',
+        label: 'FAQ',
+        items: ['other/faq/how-is-webiny-serverless'],
+      },
       {
         type: 'link',
         label: 'How To Contribute',
