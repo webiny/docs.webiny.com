1ab7c7d338865b5e0f800e5e405940fe0853489e
Pavel Denisjuk <pavel@webiny.com>
2019-11-14 19:52:00 +0100

    Sync with master.

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,27 +1,21 @@
 import React from "react";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-
 import Layout from "@theme/Layout";
 
 class Home extends React.Component {
+    componentWillMount() {
+        window.location.href = "/docs/webiny/introduction";
+    }
 
-  componentWillMount () {
-    window.location.href = "/docs/webiny/introduction"
-  }
-
-  render() {
-    return (
-      <Layout>
-        <body>
-          If you are not redirected automatically, follow
-          this{" "}
-          <a href="/docs/webiny/introduction">link</a>.
-        </body>
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <body>
+                    If you are not redirected automatically, follow this{" "}
+                    <a href="/docs/webiny/introduction">link</a>.
+                </body>
+            </Layout>
+        );
+    }
 }
 
 export default Home;
