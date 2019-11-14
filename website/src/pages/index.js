ad3fde768d3223e8d1635bc5f266cfe7cd69b5e7
Pavel Denisjuk <pavel@webiny.com>
2019-11-14 20:45:46 +0100

    fix: add handling of window object.

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Layout from "@theme/Layout";
 
-class Home extends React.Component {
-    componentWillMount() {
-        window.location.href = "/docs/webiny/introduction";
-    }
+export default () => {
+    React.useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.location.href = "/docs/webiny/introduction";
+        }
+    }, []);
 
-    render() {
-        return (
-            <Layout>
-                <body>
-                    If you are not redirected automatically, follow this{" "}
-                    <a href="/docs/webiny/introduction">link</a>.
-                </body>
-            </Layout>
-        );
-    }
-}
-
-export default Home;
+    return (
+        <Layout>
+            <body>
+                If you are not redirected automatically, follow this{" "}
+                <a href="/docs/webiny/introduction">link</a>.
+            </body>
+        </Layout>
+    );
+};
