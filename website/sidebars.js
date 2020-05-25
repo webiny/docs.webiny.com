ee1faa64442997729e2bd6f2b78e358720d694b3
Emil Kais <emil.kais94@gmail.com>
2020-05-25 10:11:03 -0400

    Put Guides beneath Getting Started.

--- website/sidebars.js
+++ website/sidebars.js
@@ -13,14 +13,14 @@ module.exports = {
                 href: "https://github.com/webiny/webiny-js/blob/master/CONTRIBUTING.md"
             }
         ],
-        "Guides": [
-          "guides/mongodb-atlas",
-          "guides/headless-gatsby-tutorial"
-        ],
         "Get Started": [
             "get-started/quick-start",
             "get-started/going-live",
             "get-started/development-principles"
+        ],
+        "Guides": [
+            "guides/mongodb-atlas",
+            "guides/headless-gatsby-tutorial"
         ],
         "Deep Dive": [
             "deep-dive/project-structure",
