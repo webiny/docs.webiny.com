a182f3723641d16f5abef2f6cfcde913fb35523f
Fsalker <Andreiii500@yahoo.com>
2020-06-22 15:44:17 +0300

    feat: add "Is Webiny Free?" FAQ article (#142)

--- website/sidebars.js
+++ website/sidebars.js
@@ -272,6 +272,7 @@ module.exports = {
         FAQ: [
             "faq/how-is-webiny-serverless",
             "faq/why-not-dynamodb",
+            "faq/is-webiny-free",
             "faq/can-i-host-webiny-on-my-laptop",
             {
                 type: "link",
