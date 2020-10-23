a2f05981891ce41267b7760f6c5ab8c2b537abf0
SvenAlHamad <sven@webiny.com>
2020-10-23 14:58:01 +0100

    Contributing docs guide done

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,11 +2,7 @@ module.exports = {
     docs: [
         {
             Contributing: [
-                {
-                    type: "category",
-                    label: "Documentation",
-                    items: ["contributing/documentation/get-started"]
-                }
+                "contributing/documentation"
             ]
         }
     ]
