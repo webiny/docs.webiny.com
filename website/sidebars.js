e5fcc66aaa2dec180b40c614d91dff491193afbd
SvenAlHamad <sven@webiny.com>
2020-10-27 16:06:51 +0000

    Added AWS guide and Install overview articles.

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,6 +2,27 @@ module.exports = {
     docs: [
         "introduction",
         {
+            Tutorials: [
+                
+                {
+                    type: "category",
+                    label: "Install Webiny",
+                    items: [
+                        "tutorials/install-webiny/overview",
+                    ]
+                },
+            ],
+            "How-to guides": [
+                {
+                    type: "category",
+                    label: "Deployment",
+                    items: [
+                        "how-to-guides/deployment/configure-aws-credentials",
+                    ]
+                },   
+            ]
+        },
+        {
             Contributing: [
                 "contributing/documentation"
             ]
