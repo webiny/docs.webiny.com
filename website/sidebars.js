8830dbbc250ecf996bf64fa41f339f874581c3d3
Adrian Smijulj <adrian1358@gmail.com>
2021-01-13 13:47:44 +0100

    wip: create Project Organisation page

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,16 +2,15 @@ module.exports = {
     docs: [
         "introduction",
         {
-            Tutorials: [
-                "tutorials/install-webiny"
-            ],
+            Tutorials: ["tutorials/install-webiny"],
             "How-to guides": [
                 {
                     type: "category",
                     label: "Deployment",
                     items: ["how-to-guides/deployment/configure-aws-credentials"]
                 }
-            ]
+            ],
+            "Key Topics": ["key-topics/project-organisation"]
         },
         {
             Contributing: ["contributing/documentation"]
