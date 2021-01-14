5cb7ae64720c32db86bbdbe280795e70040137da
albionahoti <albionahh@outlook.com>
2021-01-14 18:33:36 +0100

    add: sidebar changes

--- website/sidebars.js
+++ website/sidebars.js
@@ -6,20 +6,25 @@ module.exports = {
                 {
                     type: "category",
                     label: "Install Webiny",
-                    items: ["tutorials/install-webiny/overview"]
-                }
+                    items: ["tutorials/install-webiny/overview"],
+                },
             ],
             "How-to guides": [
                 {
                     type: "category",
                     label: "Deployment",
-                    items: ["how-to-guides/deployment/configure-aws-credentials"]
-                }
-            ]
+                    items: ["how-to-guides/deployment/configure-aws-credentials"],
+                },
+                {
+                    type: "category",
+                    label: "Headless CMS",
+                    items: ["how-to-guides/headless-cms/create-a-webiny-headless-cms-field-plugin"],
+                },
+            ],
         },
         {
-            Contributing: ["contributing/documentation"]
+            Contributing: ["contributing/documentation"],
         },
-        "webiny-telemetry"
-    ]
+        "webiny-telemetry",
+    ],
 };
