bd5eea477062ce752467607a5af64eb1f1e8de7f
Adrian Smijulj <adrian1358@gmail.com>
2021-01-15 00:11:31 +0100

    wip: create project stacks docs

--- website/sidebars.js
+++ website/sidebars.js
@@ -10,7 +10,20 @@ module.exports = {
                     items: ["how-to-guides/deployment/configure-aws-credentials"]
                 }
             ],
-            "Key Topics": ["key-topics/project-organisation"]
+            "Key Topics": [
+                "key-topics/project-organisation",
+                "key-topics/cloud-infrastructure",
+                {
+                    type: "category",
+                    label: "Project Stacks",
+                    items: [
+                        "key-topics/stacks/overview",
+                        "key-topics/stacks/deploying-with-pulumi",
+                        "key-topics/stacks/working-with-stacks-using-cli",
+                        "key-topics/stacks/environments",
+                    ]
+                }
+            ]
         },
         {
             Contributing: ["contributing/documentation"]
