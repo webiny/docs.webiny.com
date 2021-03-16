51c7afd636f085df08b8eda8951cf539e89f4f9b
Adrian Smijulj <adrian1358@gmail.com>
2021-03-16 15:13:50 +0100

    wip: create state-management guide

--- website/sidebars.js
+++ website/sidebars.js
@@ -79,6 +79,7 @@ module.exports = {
                     items: [
                         "how-to-guides/ci-cd/introduction",
                         "how-to-guides/ci-cd/developing-serverless-applications",
+                        "how-to-guides/ci-cd/cloud-infrastructure-state-files",
                         "how-to-guides/ci-cd/ci-cd",
                         "how-to-guides/ci-cd/environments",
                         "how-to-guides/ci-cd/version-control"
