8512740dd562d702d6865bb81fa4cd6981b9381a
Ashutosh <bhardwajashu96@hotmail.com>
2021-03-20 10:14:20 +0530

    tutorial: how to customize a Page builder element (#250)
    * feat: add assets
    
    * feat: add how to update an element tutorial
    
    * fix: make style improvements
    
    * fix: remove informal writing elements like emoji
    
    * fix: create function
    
    * fix: update tutorial URL
    
    * fix: rename tutorial file
    
    * fix: update as per the feedback

--- website/sidebars.js
+++ website/sidebars.js
@@ -53,7 +53,10 @@ module.exports = {
                 {
                     type: "category",
                     label: "Page Builder",
-                    items: ["tutorials/page-builder/create-a-new-page-builder-element"]
+                    items: [
+                        "tutorials/page-builder/create-a-new-page-builder-element",
+                        "tutorials/page-builder/customize-an-existing-page-builder-element"
+                    ]
                 },
                 {
                     type: "category",
@@ -130,9 +133,7 @@ module.exports = {
                         {
                             type: "category",
                             label: "Headless CMS",
-                            items: [
-                                "key-topics/webiny-applications/headless-cms/graphql-api"
-                            ]
+                            items: ["key-topics/webiny-applications/headless-cms/graphql-api"]
                         }
                     ]
                 },
