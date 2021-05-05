a9afa4e2510d42736cea08d28b6ce288e02d28a7
Adrian Smijulj <adrian1358@gmail.com>
2021-05-05 10:56:21 +0200

    wip: create development workflows articles

--- website/sidebars.js
+++ website/sidebars.js
@@ -95,12 +95,12 @@ module.exports = {
                             type: "category",
                             label: "Workflows",
                             items: [
-                                "how-to-guides/ci-cd/introduction",
-                                // "how-to-guides/ci-cd/developing-serverless-applications",
-                                "how-to-guides/ci-cd/cloud-infrastructure-state-files",
-                                // "how-to-guides/ci-cd/ci-cd",
-                                "how-to-guides/ci-cd/environments",
-                                "how-to-guides/ci-cd/version-control"
+                                "how-to-guides/development/workflows/introduction",
+                                "how-to-guides/development/workflows/developing-serverless-applications",
+                                "how-to-guides/development/workflows/cloud-infrastructure-state-files",
+                                "how-to-guides/development/workflows/ci-cd",
+                                "how-to-guides/development/workflows/environments",
+                                "how-to-guides/development/workflows/version-control"
                             ]
                         }
                     ]
