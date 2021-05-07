fc20bde6013e3aa326752d533b97db536bb52a4a
Adrian Smijulj <adrian1358@gmail.com>
2021-05-07 10:02:07 +0200

    wip: create 5.6.0 changelog

--- website/sidebars.js
+++ website/sidebars.js
@@ -257,7 +257,7 @@ module.exports = {
             Contributing: ["contributing/documentation", "contributing/new-page-template"]
         },
         {
-            Changelog: ["changelog/5.5.0", "changelog/5.4.0", "changelog/5.3.0"]
+            Changelog: ["changelog/5.6.0", "changelog/5.5.0", "changelog/5.4.0", "changelog/5.3.0"]
         },
         "webiny-telemetry"
     ]
