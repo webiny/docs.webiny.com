249a31411d169c50f56b9e5766ddf3f2d58ba19b
Bruno Zorić <bruno.zoric@gmail.com>
2021-05-21 07:29:43 +0200

    feat(how-to-guides): headless cms upgrade to 5.7.0 to 5.8.0

--- website/sidebars.js
+++ website/sidebars.js
@@ -161,6 +161,7 @@ module.exports = {
                     label: "Upgrade Webiny",
                     items: [
                         "how-to-guides/upgrade-webiny/overview",
+                        "how-to-guides/upgrade-webiny/5.7.0-to-5.8.0",
                         "how-to-guides/upgrade-webiny/5.5.0-to-5.6.0",
                         "how-to-guides/upgrade-webiny/5.4.0-to-5.5.0",
                         "how-to-guides/upgrade-webiny/5.3.0-to-5.4.0",
