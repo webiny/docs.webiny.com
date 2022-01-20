6055a8ae2b58f711700f6408309761bb0e410e06
Pavel Denisjuk <pavel@webiny.com>
2022-01-20 22:06:51 +0100

    chore: add alias for react-dom/server

--- website/plugins/aliases/index.js
+++ website/plugins/aliases/index.js
@@ -4,13 +4,18 @@ module.exports = function(context, options) {
     return {
         name: "resolve-aliases",
         configureWebpack(config) {
+            const alias = {
+                "@components": path.resolve(__dirname, "../../components"),
+                react: require.resolve("react"),
+                "react-dom/server": require.resolve("react-dom/server"),
+                "react-dom": require.resolve("react-dom")
+            };
+
+            console.log("alias", alias);
+
             return {
                 resolve: {
-                    alias: {
-                        "@components": path.resolve(__dirname, "../../components"),
-                        react: require.resolve("react"),
-                        "react-dom": require.resolve("react-dom")
-                    }
+                    alias
                 }
             };
         }
