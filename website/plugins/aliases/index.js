b25d4389d0b3dba68c705d533224a236f8239148
Pavel Denisjuk <pavel@webiny.com>
2022-01-20 22:16:57 +0100

    chore: remove alias logging

--- website/plugins/aliases/index.js
+++ website/plugins/aliases/index.js
@@ -4,18 +4,14 @@ module.exports = function(context, options) {
     return {
         name: "resolve-aliases",
         configureWebpack(config) {
-            const alias = {
-                "@components": path.resolve(__dirname, "../../components"),
-                react: require.resolve("react"),
-                "react-dom/server": require.resolve("react-dom/server"),
-                "react-dom": require.resolve("react-dom")
-            };
-
-            console.log("alias", alias);
-
             return {
                 resolve: {
-                    alias
+                    alias: {
+                        "@components": path.resolve(__dirname, "../../components"),
+                        react: require.resolve("react"),
+                        "react-dom/server": require.resolve("react-dom/server"),
+                        "react-dom": require.resolve("react-dom")
+                    }
                 }
             };
         }
