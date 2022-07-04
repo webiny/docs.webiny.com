3804124beb2db184d5680dc2c872e109d9e31ba2
Pavel Denisjuk <pavel@webiny.com>
2022-07-04 13:57:00 +0200

    feat: generate sitemap.xml on build

--- scripts/prepareDocs.js
+++ scripts/prepareDocs.js
@@ -27,6 +27,7 @@ function injectVersion(rootPath, version) {
 (async () => {
     const [latestVersion, ...versions] = allVersions;
     const pages = {};
+    const sitemap = [];
 
     await writeJsonFile(path.join(process.cwd(), "src/data/versions.json"), {
         latestVersion,
@@ -63,17 +64,42 @@ function injectVersion(rootPath, version) {
     // Save pages catalog
     await writeJsonFile(path.join(process.cwd(), "src/data/pages.json"), pages);
 
+    // Save sitemap
+    await writeSitemap();
+
     async function addPagesToCatalog(path, version) {
         const urlRegExp = new RegExp("(/docs/.*).mdx");
 
         const files = await globby(`${path}/**/*.mdx`);
         pages[version] = await pMap(files, async file => {
             const { attributes } = await frontMatter(await fs.readFile(file, "utf8"));
+            const pagePath = file.match(urlRegExp)[1];
+
+            // Add page to sitemap
+            sitemap.push(pagePath);
+
             return {
                 ...attributes,
                 version,
-                path: file.match(urlRegExp)[1]
+                path: pagePath
             };
         });
+    }
+
+    async function writeSitemap() {
+        const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${sitemap
+        .map(
+            url => `<url>
+      <loc>https://www.webiny.com${url}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>1.0</priority>
+    </url>`
+        )
+        .join("\n    ")}
+  </urlset>`;
+        await fs.writeFile(__dirname + "/../public/sitemap.xml", xml);
     }
 })();
