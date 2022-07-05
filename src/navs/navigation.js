ae874d1aeeb5f4d48243a46b32a8cf83eebd3081
Bruno Zorić <bruno.zoric@gmail.com>
2022-07-05 09:54:06 +0200

    wip: webiny 5.30.0

--- src/navs/navigation.js
+++ src/navs/navigation.js
@@ -1,7 +1,9 @@
 import { createNavigation as create528 } from "./5.28";
 import { createNavigation as create529 } from "./5.29";
+import { createNavigation as create530 } from "./5.30";
 
 export const navigation = {
     "5.28.x": create528(),
-    latest: create529("latest")
+    "5.29.x": create529(),
+    latest: create530("latest")
 };
