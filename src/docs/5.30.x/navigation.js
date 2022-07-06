3b35e109a626d930f989ae54801a946f356436d9
Pavel Denisjuk <pavel@webiny.com>
2022-07-06 10:22:53 +0200

    fix: merge master and use inheritance to extend

---
+++ src/docs/5.30.x/navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Collapsable, Page, Section } from "@/docs/utils/navigation";
+import { Navigation as BaseNavigation } from "../5.29.x/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            {/* Inherit navigation from 5.28.x. */}
+            <BaseNavigation />
+            {/* Add new items. */}
+            <Collapsable title={"Webiny Overview"}>
+                <Section title={"Features"}>
+                    <Page link={"overview/features/mailer"} />
+                </Section>
+            </Collapsable>
+        </>
+    );
+};
--- src/navs/navigation.js
+++ src/navs/navigation.js
@@ -1,9 +1,3 @@
-import { createNavigation as create528 } from "./5.28";
-import { createNavigation as create529 } from "./5.29";
-import { createNavigation as create530 } from "./5.30";
+import navigationData from "@/data/navigation.json";
 
-export const navigation = {
-    "5.28.x": create528(),
-    "5.29.x": create529(),
-    latest: create530("latest")
-};
+export const navigation = navigationData;
