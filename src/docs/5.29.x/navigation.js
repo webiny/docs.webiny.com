b4372c1ca1357a106b22f91640cd31aa4b4f8ba9
Adrian Smijulj <adrian1358@gmail.com>
2022-07-12 17:16:53 +0200

    wip: update 5.29.0 docs

--- src/docs/5.29.x/navigation.js
+++ src/docs/5.29.x/navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import { Collapsable, Page, Section } from "@/docs/utils/navigation";
 import { Navigation as BaseNavigation } from "../5.28.x/navigation";
+import { Collapsable, Section, Page } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
     return (
@@ -8,6 +8,22 @@ export const Navigation = () => {
             {/* Inherit navigation from 5.28.x. */}
             <BaseNavigation />
             {/* Add new items here. */}
+
+            <Collapsable title={"Architecture"}>
+                <Section title={"Core Architecture"} before={"API Architecture"}>
+                    <Page link={"architecture/core/overview"} />
+                </Section>
+                <Section title={"API Architecture"}>
+                    <Page link={"architecture/api/introduction"} remove />
+                    <Page link={"architecture/api/overview-vpc-default"} remove />
+                    <Page link={"architecture/api/overview-vpc-custom"} remove />
+                </Section>
+                <Section title={"Deployment Modes"} after={"Website Architecture"}>
+                    <Page link={"architecture/deployment-modes/introduction"} />
+                    <Page link={"architecture/deployment-modes/development"} />
+                    <Page link={"architecture/deployment-modes/production"} />
+                </Section>
+            </Collapsable>
         </>
     );
 };
