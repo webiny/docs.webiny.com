a3c9e3e57b4a331077bf40a2b0050f9840fd0de9
Bruno Zorić <bruno.zoric@gmail.com>
2022-08-10 14:55:26 +0200

    wip: handler and routes

---
+++ src/docs/5.31.x/navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Collapsable, Page, Section } from "@/docs/utils/navigation";
+import { Navigation as BaseNavigation } from "../5.30.x/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            {/* Inherit navigation from 5.30.x. */}
+            <BaseNavigation />
+            {/* Add new items. */}
+            <Collapsable title={"Core Development Concepts"}>
+                <Section title={"Basics"}>
+                    <Page link={"core-development-concepts/basics/routes"} />
+                </Section>
+            </Collapsable>
+        </>
+    );
+};
