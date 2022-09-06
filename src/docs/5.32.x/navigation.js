fb0ef37b61b67d79d19e672340ce21bd46207e6f
Pavel Denisjuk <pavel@webiny.com>
2022-09-06 15:54:21 +0200

    wip: add changelog for 5.32.0

---
+++ src/docs/5.32.x/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { Collapsable, Page, Section } from "@/docs/utils/navigation";
+import { Navigation as BaseNavigation } from "../5.31.x/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            {/* Inherit navigation from 5.31.x. */}
+            <BaseNavigation />
+        </>
+    );
+};
