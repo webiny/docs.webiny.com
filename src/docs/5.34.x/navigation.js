c30cf10f11bcd77da30b887f2fb2b47c8c1c00d7
Bruno Zorić <bruno.zoric@gmail.com>
2022-10-25 16:48:31 +0200

    wip(mailer): changes to the docs + release notes

---
+++ src/docs/5.34.x/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigation as BaseNavigation } from "../5.33.x/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            {/* Inherit navigation from 5.33.x. */}
+            <BaseNavigation />
+            {/* Add new items. */}
+        </>
+    );
+};
