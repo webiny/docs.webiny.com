2d542864dbb1af300f5025e6d91a96a7a391c5f6
Swapnil M Mane <swapnilmmane@gmail.com>
2022-12-08 15:05:47 +0530

    fix: corrected the documentation link for user-guides if user visited the older version guides

--- src/components/page/ViewLatestVersion.js
+++ src/components/page/ViewLatestVersion.js
@@ -4,19 +4,32 @@ import { useVersions } from "@/hooks/useVersions";
 import { Alert } from "@/components/Alert";
 
 export const ViewLatestVersion = () => {
-    const { version } = usePage();
+    const { version, type } = usePage();
     const versions = useVersions();
+
     if (version === "latest") {
         return null;
     }
 
-    return (
-        <div className={"mb-10"}>
-            <Alert type={"warning"} title={"Warning"}>
-                You&apos;re browsing the documentation for an old version of Webiny. Consider
-                upgrading your project to Webiny{" "}
-                <Link href={"/docs/get-started/install-webiny"}>{versions.latestVersion}</Link>.
-            </Alert>
-        </div>
-    );
+    if (type === "docs") {
+        return (
+            <div className={"mb-10"}>
+                <Alert type={"warning"} title={"Warning"}>
+                    You&apos;re browsing the documentation for an old version of Webiny. Consider
+                    upgrading your project to Webiny{" "}
+                    <Link href={"/docs/get-started/install-webiny"}>{versions.latestVersion}</Link>.
+                </Alert>
+            </div>
+        );
+    } else {
+        return (
+            <div className={"mb-10"}>
+                <Alert type={"warning"} title={"Warning"}>
+                    You&apos;re browsing the documentation for an old version of Webiny. Consider
+                    upgrading your project to Webiny{" "}
+                    <Link href={"/docs/user-guides/introduction"}>{versions.latestVersion}</Link>.
+                </Alert>
+            </div>
+        );
+    }
 };
