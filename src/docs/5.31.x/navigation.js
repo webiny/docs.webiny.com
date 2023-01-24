1c5d70ac3d9d37203131a23b97bdb73f0911a7e0
Bruno Zorić <bruno.zoric@gmail.com>
2023-01-24 21:58:00 +0100

    fix: add missing navigation component

--- src/docs/5.31.x/navigation.js
+++ src/docs/5.31.x/navigation.js
@@ -3,18 +3,25 @@ import { NavGroup, Collapsable, Page, Section } from "@/docs/utils/navigation";
 import { Navigation as BaseNavigation } from "../5.30.x/navigation";
 
 export const Navigation = () => {
-  return (
-    <>
-      {/* Inherit navigation from 5.30.x. */}
-      <BaseNavigation />
-      {/* Add new items. */}
-      <NavGroup type={"docs"}>
-        <Collapsable title={"Core Development Concepts"}>
-          <Section title={"Basics"}>
-            <Page link={"core-development-concepts/basics/routes-and-events"} />
-          </Section>
-        </Collapsable>
-      </NavGroup>
-    </>
-  );
+    return (
+        <>
+            {/* Inherit navigation from 5.30.x. */}
+            <BaseNavigation />
+            {/* Add new items. */}
+            <NavGroup type={"docs"}>
+                <Collapsable title={"Core Development Concepts"}>
+                    <Section title={"Basics"}>
+                        <Page link={"core-development-concepts/basics/routes-and-events"} />
+                    </Section>
+                    <Section title={"Extending and Customizing"}>
+                        <Page
+                            link={
+                                "core-development-concepts/extending-and-customizing/add-custom-route-to-existing-lambda"
+                            }
+                        />
+                    </Section>
+                </Collapsable>
+            </NavGroup>
+        </>
+    );
 };
