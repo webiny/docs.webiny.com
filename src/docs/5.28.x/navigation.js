76f5bad0005c73c0541a5a4a3cbb86b0d3f9365e
Samarpit Shrivastava <42511743+samarpit30@users.noreply.github.com>
2023-03-02 11:36:40 +0000

    Created and Reviewed first draft

--- src/docs/5.28.x/navigation.js
+++ src/docs/5.28.x/navigation.js
@@ -275,6 +275,9 @@ export const Navigation = () => {
                                     "user-guides/headless-cms/essentials/create-content-model-group"
                                 }
                             />
+                            <Page
+                                link={"user-guides/headless-cms/essentials/clone-content-model"}
+                            />
                         </Section>
                         <Section title={"Advanced"}>
                             <Page link={"user-guides/headless-cms/advanced/sample-advance-page"} />
