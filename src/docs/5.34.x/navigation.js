4471b5145046281db403b79aa4461f1abcb02a90
Swapnil M Mane <swapnilmmane@gmail.com>
2023-03-23 17:07:07 +0530

    docs: removed user guides from draft status, enable robots, redirects /docs/user-guides to /docs/user-guides

--- src/docs/5.34.x/navigation.js
+++ src/docs/5.34.x/navigation.js
@@ -61,37 +61,36 @@ export const Navigation = () => {
           </Section>
         </Collapsable>
       </NavGroup>
-      <Draft>
-        <NavGroup type={"user-guides"}>
-          <Collapsable title={"Get Started"}>
-            <Page link={"user-guides/overview"} />
-          </Collapsable>
-          <Separator />
-          <Collapsable title={"Headless CMS"}>
-            <Section title={"Essentials"}>
-              <Page title={"Glossary"} link={"user-guides/headless-cms/essentials/glossary"} />
-              <Page link={"user-guides/headless-cms/essentials/create-content-model"} />
-              <Page link={"user-guides/headless-cms/essentials/create-content-entry"} />
-              <Page link={"user-guides/headless-cms/essentials/create-content-model-group"} />
-              <Page link={"user-guides/headless-cms/essentials/content-entry-revisions"} />
-              <Page link={"user-guides/headless-cms/essentials/clone-content-model"} />
-              <Page link={"user-guides/headless-cms/essentials/manage-content-model-settings"} />
-              <Page link={"user-guides/headless-cms/essentials/add-validator-to-fields"} />
-              <Page link={"user-guides/headless-cms/essentials/reference-field"} />
-            </Section>
-            <Section title={"Advanced"}>
-              <Page link={"user-guides/headless-cms/advanced/content-localization"} />
-              <Page link={"user-guides/headless-cms/advanced/user-creation"} />
-            </Section>
-          </Collapsable>
-          <Separator />
-          <Collapsable title={"Page Builder"}>
-            <Section title={"Essentials"}>
-              <Page title={"Glossary"} link={"user-guides/page-builder/essentials/glossary"} />
-            </Section>
-          </Collapsable>
-        </NavGroup>
-      </Draft>
+
+      <NavGroup type={"user-guides"}>
+        <Collapsable title={"Get Started"}>
+          <Page link={"user-guides/overview"} />
+        </Collapsable>
+        <Separator />
+        <Collapsable title={"Headless CMS"}>
+          <Section title={"Essentials"}>
+            <Page title={"Glossary"} link={"user-guides/headless-cms/essentials/glossary"} />
+            <Page link={"user-guides/headless-cms/essentials/create-content-model"} />
+            <Page link={"user-guides/headless-cms/essentials/create-content-entry"} />
+            <Page link={"user-guides/headless-cms/essentials/create-content-model-group"} />
+            <Page link={"user-guides/headless-cms/essentials/content-entry-revisions"} />
+            <Page link={"user-guides/headless-cms/essentials/clone-content-model"} />
+            <Page link={"user-guides/headless-cms/essentials/manage-content-model-settings"} />
+            <Page link={"user-guides/headless-cms/essentials/add-validator-to-fields"} />
+            <Page link={"user-guides/headless-cms/essentials/reference-field"} />
+          </Section>
+          <Section title={"Advanced"}>
+            <Page link={"user-guides/headless-cms/advanced/content-localization"} />
+            <Page link={"user-guides/headless-cms/advanced/user-creation"} />
+          </Section>
+        </Collapsable>
+        <Separator />
+        <Collapsable title={"Page Builder"}>
+          <Section title={"Essentials"}>
+            <Page title={"Glossary"} link={"user-guides/page-builder/essentials/glossary"} />
+          </Section>
+        </Collapsable>
+      </NavGroup>
     </>
   );
 };
