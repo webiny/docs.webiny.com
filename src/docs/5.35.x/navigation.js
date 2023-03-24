5522949702ec00ff4136c2f6645f8139fdd54fde
Bruno Zorić <bruno.zoric@gmail.com>
2023-03-24 10:02:51 +0100

    feat: headless cms reference field

--- src/docs/5.35.x/navigation.js
+++ src/docs/5.35.x/navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navigation as BaseNavigation } from "../5.34.x/navigation";
+import { Collapsable, NavGroup, Page, Section } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
     return (
@@ -7,6 +8,13 @@ export const Navigation = () => {
             {/* Inherit navigation from 5.34.x. */}
             <BaseNavigation />
             {/* Add new items. */}
+            <NavGroup type={"docs"}>
+                <Collapsable title={"Headless CMS"}>
+                    <Section title={"Fields"}>
+                        <Page link={"headless-cms/fields/reference"} />
+                    </Section>
+                </Collapsable>
+            </NavGroup>
         </>
     );
 };
