d14e2b79692f028db1911523b797fec790fe1b07
Swapnil M Mane <swapnilmmane@gmail.com>
2023-05-18 18:02:57 +0530

    docs: moved user guides from 5.34.x to 5.35.x

--- src/docs/5.34.x/navigation.js
+++ src/docs/5.34.x/navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigation as BaseNavigation } from "../5.33.x/navigation";
-import { Page, Section, Collapsable, NavGroup, Separator } from "@/docs/utils/navigation";
+import { Page, Section, Collapsable, NavGroup } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
   return (
@@ -70,41 +70,6 @@ export const Navigation = () => {
         </Collapsable>
       </NavGroup>
 
-      <NavGroup type={"user-guides"}>
-        <Collapsable title={"Get Started"}>
-          <Page link={"user-guides/overview"} />
-        </Collapsable>
-        <Separator />
-        <Collapsable title={"Headless CMS"}>
-          <Section title={"Essentials"}>
-            <Page title={"Glossary"} link={"user-guides/headless-cms/essentials/glossary"} />
-            <Page link={"user-guides/headless-cms/essentials/create-content-model"} />
-            <Page link={"user-guides/headless-cms/essentials/create-content-entry"} />
-            <Page link={"user-guides/headless-cms/essentials/create-content-model-group"} />
-            <Page link={"user-guides/headless-cms/essentials/content-entry-revisions"} />
-            <Page link={"user-guides/headless-cms/essentials/clone-content-model"} />
-            <Page link={"user-guides/headless-cms/essentials/manage-content-model-settings"} />
-            <Page link={"user-guides/headless-cms/essentials/add-validator-to-fields"} />
-            <Page link={"user-guides/headless-cms/essentials/reference-field"} />
-          </Section>
-          <Section title={"Advanced"}>
-            <Page link={"user-guides/headless-cms/advanced/content-localization"} />
-            <Page link={"user-guides/headless-cms/advanced/user-creation"} />
-          </Section>
-        </Collapsable>
-        <Separator />
-        <Collapsable title={"Page Builder"}>
-          <Section title={"Essentials"}>
-            <Page title={"Glossary"} link={"user-guides/page-builder/essentials/glossary"} />
-            <Page title={"Overview"} link={"user-guides/page-builder/essentials/overview"} />
-            <Page title={"Training Course"} link={"user-guides/page-builder/essentials/training-course"} />
-            <Page link={"user-guides/page-builder/essentials/create-page"} />
-            <Page link={"user-guides/page-builder/essentials/create-a-page-block-from-figma-design"} />
-            <Page link={"user-guides/page-builder/essentials/seo-and-social-sharing"} />
-            <Page link={"user-guides/page-builder/essentials/navigation-menu"} />
-          </Section>
-        </Collapsable>
-      </NavGroup>
     </>
   );
 };
