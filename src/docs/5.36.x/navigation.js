b057e2d55586e553c67d5f9e80cd5a4779d20f98
adrians5j <adrian@webiny.com>
2023-05-19 11:29:04 +0200

    fix: add missing navigation item

--- src/docs/5.36.x/navigation.js
+++ src/docs/5.36.x/navigation.js
@@ -3,19 +3,16 @@ import { Navigation as BaseNavigation } from "../5.28.x/navigation";
 import { Collapsable, Section, Page, NavGroup } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
-  return (
-    <>
-      <BaseNavigation />
-      <NavGroup type={"docs"}>
-        <Collapsable title={"Infrastructure"}>
-          <Section title={"Basics"}>
-            <Page
-              link={"infrastructure/basics/modify-cloud-infrastructure"}
-              after={"infrastructure/basics/destroy-cloud-infrastructure"}
-            />
-          </Section>
-        </Collapsable>
-      </NavGroup>
-    </>
-  );
+    return (
+        <>
+            <BaseNavigation />
+            <NavGroup type={"docs"}>
+                <Collapsable title={"Infrastructure"}>
+                    <Section title={"Basics"}>
+                        <Page link={"infrastructure/basics/modify-cloud-infrastructure"} />
+                    </Section>
+                </Collapsable>
+            </NavGroup>
+        </>
+    );
 };
