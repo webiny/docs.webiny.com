efa4ae24ea8a14c3bb6f45fd1fd9ebd7f52f3a8f
adrians5j <adrian@webiny.com>
2023-05-19 09:25:06 +0200

    fix: update modify cloud infra doc

---
+++ src/docs/5.36.x/navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Navigation as BaseNavigation } from "../5.28.x/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            <BaseNavigation />
+        </>
+    );
+};
