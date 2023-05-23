07b371cf7f1460e674d234e04dd206fe26bd5fa1
Leonardo Giacone <leonardo@webiny.com>
2023-05-23 14:17:33 +0200

    feat: 5.36.0 changelog (#592)
    * feat: add file manager aco release note
    
    * fix: fm-aco-overview extension
    
    * fix: update modify cloud infra doc
    
    * fix: update modify cloud infra doc
    
    * fix: add missing navigation item
    
    * fix: add missing navigation item
    
    * fix: correct string
    
    * fix: correct string
    
    * feat: add file manager and page builder features and fixes
    
    * feat: create 5.36.0 overview file manager page
    
    * chore: move deployments to the bottom of the list
    
    * feat: add "Button Page Element - Icon Position CSS Class"
    
    ---------
    
    Co-authored-by: adrians5j <adrian@webiny.com>

---
+++ src/docs/5.36.x/navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigation as BaseNavigation } from "../5.28.x/navigation";
+import { Collapsable, Section, Page, NavGroup } from "@/docs/utils/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            <BaseNavigation />
+            <NavGroup type={"docs"}>
+                <Collapsable title={"Infrastructure"}>
+                    <Section title={"Basics"}>
+                        <Page link={"infrastructure/basics/modify-cloud-infrastructure"} />
+                    </Section>
+                </Collapsable>
+            </NavGroup>
+        </>
+    );
+};
