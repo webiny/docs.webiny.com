92c1381e27fca72d5f145e965c68e98838a51527
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-05-24 09:46:38 +0200

    fix: include user docs navigation into 5.36.x docs

--- src/docs/5.36.x/navigation.js
+++ src/docs/5.36.x/navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigation as BaseNavigation } from "../5.28.x/navigation";
+import { Navigation as BaseNavigation } from "../5.35.x/navigation";
 import { Collapsable, Section, Page, NavGroup } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
