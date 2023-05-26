6eeae215d0679d3d990ad71ae1d1b7ae490310e3
adrians5j <adrian@webiny.com>
2023-05-26 10:10:59 +0200

    fix: remove redundant link

--- src/docs/5.36.x/navigation.js
+++ src/docs/5.36.x/navigation.js
@@ -1,18 +1,10 @@
 import React from "react";
 import { Navigation as BaseNavigation } from "../5.35.x/navigation";
-import { Collapsable, Section, Page, NavGroup } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
     return (
         <>
             <BaseNavigation />
-            <NavGroup type={"docs"}>
-                <Collapsable title={"Infrastructure"}>
-                    <Section title={"Basics"}>
-                        <Page link={"infrastructure/basics/modify-cloud-infrastructure"} />
-                    </Section>
-                </Collapsable>
-            </NavGroup>
         </>
     );
 };
