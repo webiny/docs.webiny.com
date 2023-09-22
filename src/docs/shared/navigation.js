1564f830a9719957d85acb818c224cbf89a17be1
adrians5j <adrian@webiny.com>
2023-09-22 09:01:49 +0200

    feat: create CW article

--- src/docs/shared/navigation.js
+++ src/docs/shared/navigation.js
@@ -37,7 +37,7 @@ export const Navigation = () => {
 
           {/* Messages */}
           <Page link={"messages/lambda-edge-warning"} hidden={true}/>
-          <Page link={"messages/how-to-cloudwatch"} hidden={true}/>
+          <Page link={"messages/cloudwatch"} hidden={true}/>
         </NavGroup>
     );
 };
