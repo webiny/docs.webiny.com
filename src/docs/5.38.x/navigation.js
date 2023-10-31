f8b63ee451d58be25a040d3b9dbfea3d91a5b329
Leonardo Giacone <leonardo.giacone@gmail.com>
2023-10-31 08:23:45 +0100

    feat: add HCMS bulk actions article

--- src/docs/5.38.x/navigation.js
+++ src/docs/5.38.x/navigation.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { Navigation as BaseNavigation } from "../5.36.x/navigation";
+import { Navigation as BaseNavigation } from "../5.37.x/navigation";
 import { Page, Section, Collapsable, NavGroup } from "@/docs/utils/navigation";
 
 export const Navigation = () => {
     return (
         <>
-            {/* Inherit navigation from 5.36.x. */}
+            {/* Inherit navigation from 5.37.x. */}
             <BaseNavigation />
             {/* Add new items. */}
             <NavGroup type={"docs"}>
+                <Collapsable title={"Headless CMS"}>
+                    <Section title={"Extending functionality"}>
+                        <Page
+                          link={"headless-cms/extending/customize-entry-list-bulk-actions"}
+                          after={"headless-cms/extending/customize-entry-list-filters"}
+                        />
+                    </Section>
+                </Collapsable>
                 <Collapsable title={"Webiny Enterprise"}>
                     <Page link={"enterprise/teams"} remove />
                     <Section title={"Advanced Access Control Layer (AACL)"}>
