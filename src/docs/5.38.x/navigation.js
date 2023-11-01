8b7c8819cd3d497373d16af7a29d3e11a8ce5595
Pavel Denisjuk <pavel@webiny.com>
2023-11-01 15:51:50 +0100

    wip: lexical tools article

--- src/docs/5.38.x/navigation.js
+++ src/docs/5.38.x/navigation.js
@@ -12,40 +12,51 @@ export const Navigation = () => {
                 <Collapsable title={"Page Builder"}>
                     <Section title={"Extending functionality"}>
                         <Page
-                          link={"page-builder/extending/customize-page-bulk-actions"}
-                          before={"page-builder/extending/create-a-page-element"}
+                            link={"page-builder/extending/customize-page-bulk-actions"}
+                            before={"page-builder/extending/create-a-page-element"}
                         />
                     </Section>
                 </Collapsable>
                 <Collapsable title={"Headless CMS"}>
                     <Section title={"Extending functionality"}>
                         <Page
-                          link={"headless-cms/extending/customize-entry-list-bulk-actions"}
-                          after={"headless-cms/extending/customize-entry-list-filters"}
+                            link={"headless-cms/extending/customize-entry-list-bulk-actions"}
+                            after={"headless-cms/extending/customize-entry-list-filters"}
                         />
                     </Section>
                 </Collapsable>
-              <Collapsable title={"File Manager"}>
-                <Section title={"Extending functionality"}>
-                  <Page
-                    link={"file-manager/extending/customize-file-bulk-actions"}
-                    after={"file-manager/extending/customize-file-filters"}
-                  />
-                </Section>
-              </Collapsable>
-              <Collapsable title={"Webiny Enterprise"}>
-                  <Page link={"enterprise/teams"} remove />
-                  <Section title={"Advanced Access Control Layer (AACL)"}>
-                      <Page link={"enterprise/aacl/introduction"} />
-                      <Page link={"enterprise/aacl/teams"} />
-                      <Page link={"enterprise/aacl/folder-level-permissions"} />
-                  </Section>
-              </Collapsable>
+                <Collapsable title={"File Manager"}>
+                    <Section title={"Extending functionality"}>
+                        <Page
+                            link={"file-manager/extending/customize-file-bulk-actions"}
+                            after={"file-manager/extending/customize-file-filters"}
+                        />
+                    </Section>
+                </Collapsable>
+                <Collapsable title={"Webiny Enterprise"}>
+                    <Page link={"enterprise/teams"} remove />
+                    <Section title={"Advanced Access Control Layer (AACL)"}>
+                        <Page link={"enterprise/aacl/introduction"} />
+                        <Page link={"enterprise/aacl/teams"} />
+                        <Page link={"enterprise/aacl/folder-level-permissions"} />
+                    </Section>
+                </Collapsable>
+                <Collapsable title={"Core Development Concepts"}>
+                    <Section title={"Extending and Customizing"}>
+                        <Page
+                            link={
+                                "core-development-concepts/extending-and-customizing/lexical-tools"
+                            }
+                        />
+                    </Section>
+                </Collapsable>
             </NavGroup>
             <NavGroup type={"user-guides"}>
                 <Collapsable title={"Headless CMS"}>
                     <Section title={"Advanced"}>
-                        <Page link={"user-guides/headless-cms/advanced/import-export-content-models"} />
+                        <Page
+                            link={"user-guides/headless-cms/advanced/import-export-content-models"}
+                        />
                     </Section>
                 </Collapsable>
             </NavGroup>
