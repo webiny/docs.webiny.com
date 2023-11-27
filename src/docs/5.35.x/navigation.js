67bd60330d1362589bad9dad62d012c69ab3ef13
Pavel Denisjuk <pavel@webiny.com>
2023-11-27 12:04:08 +0100

    wip: make page visible [no ci]

--- src/docs/5.35.x/navigation.js
+++ src/docs/5.35.x/navigation.js
@@ -11,7 +11,7 @@ export const Navigation = () => {
             <NavGroup type={"docs"}>
                 <Collapsable title={"Webiny Overview"}>
                     <Section title={"Features"}>
-                        <Page link={"overview/features/advanced-content-organization"} after={"CLI"} />
+                        <Page link={"overview/features/advanced-content-organization"} />
                     </Section>
                 </Collapsable>
             </NavGroup>
@@ -22,14 +22,27 @@ export const Navigation = () => {
                 <Separator />
                 <Collapsable title={"Headless CMS"}>
                     <Section title={"Essentials"}>
-                        <Page title={"Glossary"} link={"user-guides/headless-cms/essentials/glossary"} />
+                        <Page
+                            title={"Glossary"}
+                            link={"user-guides/headless-cms/essentials/glossary"}
+                        />
                         <Page link={"user-guides/headless-cms/essentials/create-content-model"} />
                         <Page link={"user-guides/headless-cms/essentials/create-content-entry"} />
-                        <Page link={"user-guides/headless-cms/essentials/create-content-model-group"} />
-                        <Page link={"user-guides/headless-cms/essentials/content-entry-revisions"} />
+                        <Page
+                            link={"user-guides/headless-cms/essentials/create-content-model-group"}
+                        />
+                        <Page
+                            link={"user-guides/headless-cms/essentials/content-entry-revisions"}
+                        />
                         <Page link={"user-guides/headless-cms/essentials/clone-content-model"} />
-                        <Page link={"user-guides/headless-cms/essentials/manage-content-model-settings"} />
-                        <Page link={"user-guides/headless-cms/essentials/add-validator-to-fields"} />
+                        <Page
+                            link={
+                                "user-guides/headless-cms/essentials/manage-content-model-settings"
+                            }
+                        />
+                        <Page
+                            link={"user-guides/headless-cms/essentials/add-validator-to-fields"}
+                        />
                         <Page link={"user-guides/headless-cms/essentials/reference-field"} />
                     </Section>
                     <Section title={"Advanced"}>
@@ -40,11 +53,24 @@ export const Navigation = () => {
                 <Separator />
                 <Collapsable title={"Page Builder"}>
                     <Section title={"Essentials"}>
-                        <Page title={"Glossary"} link={"user-guides/page-builder/essentials/glossary"} />
-                        <Page title={"Overview"} link={"user-guides/page-builder/essentials/overview"} />
-                        <Page title={"Training Course"} link={"user-guides/page-builder/essentials/training-course"} />
+                        <Page
+                            title={"Glossary"}
+                            link={"user-guides/page-builder/essentials/glossary"}
+                        />
+                        <Page
+                            title={"Overview"}
+                            link={"user-guides/page-builder/essentials/overview"}
+                        />
+                        <Page
+                            title={"Training Course"}
+                            link={"user-guides/page-builder/essentials/training-course"}
+                        />
                         <Page link={"user-guides/page-builder/essentials/create-page"} />
-                        <Page link={"user-guides/page-builder/essentials/create-a-page-block-from-figma-design"} />
+                        <Page
+                            link={
+                                "user-guides/page-builder/essentials/create-a-page-block-from-figma-design"
+                            }
+                        />
                         <Page link={"user-guides/page-builder/essentials/website-settings"} />
                         <Page link={"user-guides/page-builder/essentials/seo-and-social-sharing"} />
                         <Page link={"user-guides/page-builder/essentials/navigation-menu"} />
@@ -60,7 +86,9 @@ export const Navigation = () => {
                 <Collapsable title={"Form Builder"}>
                     <Section title={"Essentials"}>
                         <Page link={"user-guides/form-builder/essentials/create-form"} />
-                        <Page link={"user-guides/form-builder/essentials/using-reCAPTCHA-in-form"} />
+                        <Page
+                            link={"user-guides/form-builder/essentials/using-reCAPTCHA-in-form"}
+                        />
                     </Section>
                 </Collapsable>
             </NavGroup>
