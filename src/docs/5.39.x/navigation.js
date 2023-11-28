2f74ceaec954aecd9a1f6a521b9c4e0c8c521b64
Bruno Zorić <bruno.zoric@gmail.com>
2023-11-28 09:55:52 +0100

    fix: remove unnecessary pages from docs

--- src/docs/5.39.x/navigation.js
+++ src/docs/5.39.x/navigation.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Collapsable, NavGroup, Page, Section } from "@/docs/utils/navigation";
 import { Navigation as BaseNavigation } from "../5.38.x/navigation";
 
 export const Navigation = () => {
@@ -7,21 +6,6 @@ export const Navigation = () => {
         <>
             {/* Inherit navigation from 5.38.x. */}
             <BaseNavigation />
-            {/* Add new items. */}
-            <NavGroup type={"docs"}>
-                <Collapsable title={"Core Development Concepts"}>
-                    <Section title={"Basics"}>
-                        <Page link={"core-development-concepts/basics/routes-and-events"} />
-                    </Section>
-                    <Section title={"Extending and Customizing"}>
-                        <Page
-                            link={
-                                "core-development-concepts/extending-and-customizing/add-custom-route-to-existing-lambda"
-                            }
-                        />
-                    </Section>
-                </Collapsable>
-            </NavGroup>
         </>
     );
 };
