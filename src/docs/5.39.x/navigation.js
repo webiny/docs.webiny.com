405ebd9c4a4d3bdfdfebd63698470e6f46bd54a3
Bruno Zorić <bruno.zoric@gmail.com>
2023-11-29 09:24:46 +0100

    feat: 5.39.0 routes and events (#634)

---
+++ src/docs/5.39.x/navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navigation as BaseNavigation } from "../5.38.x/navigation";
+
+export const Navigation = () => {
+    return (
+        <>
+            {/* Inherit navigation from 5.38.x. */}
+            <BaseNavigation />
+        </>
+    );
+};
