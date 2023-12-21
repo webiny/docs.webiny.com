130e03ee18407285c9c8364efbea23cb27644b60
SvenAlHamad <sven@webiny.com>
2023-12-21 11:46:52 +0000

    Company handbook wip

--- docs/handbook/navigation.tsx
+++ docs/handbook/navigation.tsx
@@ -5,6 +5,16 @@ export const Navigation = () => {
   return (
     <NavigationRoot directory={__dirname}>
       <Group title={"Welcome!"}>
+        
+        <Group title={"About the company"} linkPrefix={"company"}>
+          <Page file={"./company/why-webiny-exists"} />
+          <Page file={"./company/how-we-got-here"} />
+          <Page file={"./company/who-are-we-building-for"} />
+          <Page file={"./company/how-we-make-users-happy"} />
+          <Page file={"./company/how-we-make-money"} />
+          <Page file={"./company/our-ethos"} />
+        </Group>
+
         <Page file={"./introduction"} link={"introduction"} />
         <Group title={"Engineering"} linkPrefix={"engineering"}>
           <Page file={"./engineering/cypress"} />
