6fc1adc8ffd717cf66dbfd9032aeed0234905f76
Swapnil M Mane <swapnilmmane@gmail.com>
2023-12-28 14:56:40 +0530

    docs: added the file manager user guides - File Upload - Organizing Files - Tagging Files - File Aliases - Bulk Actions

--- docs/user-guides/5.38.x/navigation.tsx
+++ docs/user-guides/5.38.x/navigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Page, NavigationRoot, Group } from "@webiny/docs-generator";
+import { Page, NavigationRoot, Group, Separator } from "@webiny/docs-generator";
 import { Navigation as BaseNavigation } from "../5.37.x/navigation";
 
 export const Navigation = () => {
@@ -11,6 +11,19 @@ export const Navigation = () => {
             <Page link={"headless-cms/advanced/import-export-content-models"} />
           </Group>
         </Group>
+
+        <Separator />
+
+        <Group title={"File Manager"}>
+          <Group title={"Essentials"}>
+            <Page link={"file-manager/essentials/upload-file"} />
+            <Page link={"file-manager/essentials/organizing-files"} />
+            <Page link={"file-manager/essentials/tagging-files"} />
+            <Page link={"file-manager/essentials/file-aliases"} />
+            <Page link={"file-manager/essentials/bulk-actions"} />
+        </Group>
+      </Group>
+
       </NavigationRoot>
     </BaseNavigation>
   );
