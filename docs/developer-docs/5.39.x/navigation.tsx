bc161ed2af700e2bca040195152069a4ff318b2b
Bruno Zorić <bruno.zoric@gmail.com>
2024-01-30 20:50:34 +0100

    refactor: background tasks wip

--- docs/developer-docs/5.39.x/navigation.tsx
+++ docs/developer-docs/5.39.x/navigation.tsx
@@ -46,8 +46,20 @@ export const Navigation = () => {
           </Group>
         </Group>
         <Group title={"Core Development Concepts"}>
-          <Group title={"Basics"}>
-            <Page link={"core-development-concepts/basics/background-tasks"} />
+          <Group title={"Background Tasks"}>
+            <Page link={"core-development-concepts/background-tasks/about-background-tasks"} />
+            <Page link={"core-development-concepts/background-tasks/how-background-tasks-work"} />
+            <Page
+              link={"core-development-concepts/background-tasks/how-to-define-a-background-task"}
+            />
+            <Page
+              link={
+                "core-development-concepts/background-tasks/how-to-handle-a-background-task-run"
+              }
+            />
+            <Page link={"core-development-concepts/background-tasks/background-task-management"} />
+            <Page link={"core-development-concepts/background-tasks/built-in-background-tasks"} />
+            <Page link={"core-development-concepts/background-tasks/what-to-be-careful-about"} />
           </Group>
         </Group>
       </NavigationRoot>
