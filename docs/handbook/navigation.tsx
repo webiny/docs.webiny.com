303605f72c78f940c587e9aa3059d9439a8476d7
SvenAlHamad <sven@webiny.com>
2024-02-07 16:15:48 +0000

    Updated the company handbook

--- docs/handbook/navigation.tsx
+++ docs/handbook/navigation.tsx
@@ -8,11 +8,11 @@ export const Navigation = () => {
         
         <Group title={"About the company"} linkPrefix={"company"}>
           <Page file={"./company/why-webiny-exists"} />
+          <Page file={"./company/our-values"} />
           <Page file={"./company/how-we-got-here"} />
           <Page file={"./company/who-are-we-building-for"} />
           <Page file={"./company/how-we-make-users-happy"} />
           <Page file={"./company/how-we-make-money"} />
-          <Page file={"./company/our-ethos"} />
           <Page file={"./company/deciding-what-features-to-build"} />
           <Page file={"./company/how-we-ensure-quality"} />
           <Page file={"./company/why-we-chose-serverless"} />
