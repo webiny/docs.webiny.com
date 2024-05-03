647860a977dc8cf89cc21cc8e9fc5153177db0ea
Swapnil M Mane <swapnilmmane@gmail.com>
2024-05-03 16:08:03 +0530

    docs: added various security, form builder & headless CMS user guides

--- docs/user-guides/5.39.x/navigation.tsx
+++ docs/user-guides/5.39.x/navigation.tsx
@@ -1,1 +1,40 @@
-export { Navigation } from "../5.38.x/navigation";
+import React from "react";
+import { Page, NavigationRoot, Group, Separator } from "@webiny/docs-generator";
+import { Navigation as BaseNavigation } from "../5.38.x/navigation";
+
+export const Navigation = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <BaseNavigation>
+      <NavigationRoot directory={__dirname}>
+
+      <Group title={"Headless CMS"}>
+        <Group title={"Essentials"}>
+          <Page link={"headless-cms/essentials/advanced-search-filter-builder"} />
+        </Group>
+      </Group>
+
+      <Separator />
+
+      <Group title={"Security"}>
+        <Group title={"Essentials"}>
+        <Page link={"security/essentials/user-creation"} />
+        <Page link={"security/essentials/team-creation"} />
+        <Page link={"security/essentials/assign-team"} />
+        <Page link={"security/essentials/role-creation"} />
+        <Page link={"security/essentials/assign-role"} />
+        <Page link={"security/essentials/api-key-creation"} />
+        <Page link={"security/essentials/folder-level-permission-in-headless-cms"} />
+        </Group>
+      </Group>
+
+      <Group title={"Form Builder"}>
+        <Group title={"Advanced"}>
+          <Page link={"form-builder/advanced/email-triggers"} />
+          <Page link={"form-builder/advanced/multi-steps-form"} />
+        </Group>
+      </Group>
+      {children}
+      </NavigationRoot>
+    </BaseNavigation>
+  );
+};
