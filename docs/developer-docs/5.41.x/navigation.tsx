2eb74484a5d359014990022bcbeba327b885016f
adrians5j <adrian@webiny.com>
2024-10-04 16:44:11 +0200

    feat: create 5.41.0 docs

--- docs/developer-docs/5.41.x/navigation.tsx
+++ docs/developer-docs/5.41.x/navigation.tsx
@@ -12,7 +12,7 @@ export const Navigation = () => {
         {/* Add new groups and pages here. */}
         <Group title={"Core Development Concepts"}>
           <Group title={"Security Framework"}>
-            <Page link={"core-development-concepts/security-framework/programmatic-creation-of-roles-and-teams"} />
+            <Page link={"core-development-concepts/security-framework/programmatically-create-roles-and-teams"} />
           </Group>
         </Group>
       </NavigationRoot>
