8c8ef7254bed9f69f633176c73ae2c369a72ce6d
adrians5j <adrian@webiny.com>
2024-10-14 11:57:21 +0200

    fix: update licenses on Theme Manager

--- src/components/CanIUseThis.js
+++ src/components/CanIUseThis.js
@@ -1,14 +1,31 @@
 import React from "react";
 import { Alert } from "@/components/Alert";
 
-export const CanIUseThis = ({ since, enterprise = false, children }) => {
+const LicenseMessage = ({ enterprise, business }) => {
+    if (enterprise) {
+        return (
+            <p>
+                Webiny Enterprise license is required to use this feature.
+            </p>
+        );
+    }
+
+    if (business) {
+        return (
+            <p>
+                Webiny Business license is required to use this feature. To get started, create
+                a&nbsp;
+                <a href={"https://app.webiny.com"}>Webiny Control Panel (WCP)</a> account and{" "}
+                <a href={"/docs/wcp/link-a-project"}>link your Webiny project</a>.
+            </p>
+        );
+    }
+};
+
+export const CanIUseThis = ({ since, enterprise = false, business = false, children }) => {
     return (
         <Alert type="info" title="Can I use this?">
-            {enterprise ? (
-                <p>
-                    <strong>Webiny Enterprise license is required to use this feature.</strong>
-                </p>
-            ) : null}
+            <LicenseMessage enterprise={enterprise} business={business} />
             {since ? (
                 <p>
                     This feature is available since Webiny <strong>v{since}</strong>.
