fbcf35132aeeda551675a425f4d820ae7315ee9a
Adrian Smijulj <adrian1358@gmail.com>
2024-10-17 13:04:05 +0200

    feat: create 5.41.0 release notes (#698)
    * feat: create 5.41.0 release notes
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * fix: vertically center tag
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: add nodejs and ts upgrade notes
    
    * refactor: elasticsearch shared indexes doc
    
    * docs: add hcms background tasks
    
    * docs(HCMS): change background task name
    
    * docs(HCMS): change background task name
    
    * docs(HCMS): change background task name
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * fix: code changes for es deployments
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * feat: create 5.41.0 docs
    
    * docs: upgrade bulk action articles using extensions script
    
    * fix: update licenses on Theme Manager
    
    * docs: upgrade bulk action articles providing bg task example
    
    * docs: fix 5.40.x links
    
    * fix: update licenses on Theme Manager
    
    * docs: add bulk action breaking change
    
    ---------
    
    Co-authored-by: Bruno Zorić <bruno.zoric@gmail.com>
    Co-authored-by: Leonardo Giacone <leonardo.giacone@gmail.com>

---
+++ docs/developer-docs/5.41.x/navigation.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Navigation as BaseNavigation } from "../5.40.x/navigation";
+import { Group, NavigationRoot, Page } from "@webiny/docs-generator";
+
+export const Navigation = () => {
+  return (
+    <>
+      {/* Inherit navigation from 5.40.x. */}
+      <BaseNavigation />
+      {/* Add new items. */}
+      <NavigationRoot directory={__dirname}>
+        <Group
+          title={"Security"}
+          after={"File Manager"}
+          icon={"/docs-menu-icons/wcp.svg"}
+          link={"security/overview"}
+        >
+          <Page link={"security/overview"} hidden={true} />
+          <Group title={"Security Framework"}>
+            <Page link={"security/security-framework/introduction"} />
+            <Page link={"security/security-framework/api-security"} />
+            <Page link={"security/security-framework/react-security"} />
+          </Group>
+          <Group title={"Extensions"}>
+            <Page link={"security/extensions/programmatically-create-roles-and-teams"} />
+          </Group>
+        </Group>
+        <Group title={"Core Development Concepts"}>
+          <Group title={"Security Framework"} remove>
+            <Page link={"security/security-framework/introduction"} remove />
+            <Page link={"security/security-framework/api-security"} remove />
+            <Page link={"security/security-framework/react-security"} remove />
+          </Group>
+          <Group title={"Extending and Customizing"}>
+            <Page
+              link={
+                "core-development-concepts/extending-and-customizing/how-to-customize-elasticsearch-index"
+              }
+            />
+          </Group>
+        </Group>
+        <Group title={"Webiny Enterprise"}>
+          <Group title={"Advanced Access Control Layer (AACL)"}>
+            <Page link={"enterprise/aacl/teams"} />
+          </Group>
+        </Group>
+      </NavigationRoot>
+    </>
+  );
+};
