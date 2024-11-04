cce964cc78a5a76d4d441181eb117a3522a9475b
adrians5j <adrian@webiny.com>
2024-11-04 16:07:48 +0100

    wip: create 5.41.1 release notes

--- docs/messages/navigation.tsx
+++ docs/messages/navigation.tsx
@@ -4,8 +4,9 @@ import { NavigationRoot, Page } from "@webiny/docs-generator";
 export const Navigation = () => {
   return (
     <NavigationRoot directory={__dirname} linkPrefix={"messages"}>
-      <Page link={"./lambda-edge-warning"} hidden={true} />
       <Page link={"./cloudwatch"} hidden={true} />
+      <Page link={"./lambda-edge-warning"} hidden={true} />
+      <Page link={"./missing-stack-output"} hidden={true} />
     </NavigationRoot>
   );
 };
