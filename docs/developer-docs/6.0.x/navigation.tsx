07245dc56644e9f890e8aa341abfe4ee3ae381a6
Bruno Zorić <bruno.zoric@gmail.com>
2026-02-05 12:49:00 +0100

    chore: private model

--- docs/developer-docs/6.0.x/navigation.tsx
+++ docs/developer-docs/6.0.x/navigation.tsx
@@ -32,6 +32,8 @@ export const Navigation = ({ children }: { children: React.ReactNode }) => {
 
         <Page link={"headless-cms/builder/group"} />
         <Page link={"headless-cms/builder/model"} />
+
+        <Page link={"headless-cms/examples/private-model"} />
       </Group>
       <Group title={"GraphQL"} icon={"/docs-menu-icons/webiny-overview.svg"} link={"graphql/about"}>
         <Page link={"graphql/about"} />
