9b1b8d8f2f0c4c0574dbfea5584e985514618bf8
Bruno Zorić <bruno.zoric@gmail.com>
2026-02-16 11:33:48 +0100

    wip: ai docs

--- scripts/dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-ARGS=$@
-NODE_ENV=development yarn generate $ARGS && concurrently --prefix=none "NODE_ENV=development yarn generate $ARGS --watchOnly" "next dev & sleep 2 && open http://localhost:3000/docs/get-started/install-webiny"
