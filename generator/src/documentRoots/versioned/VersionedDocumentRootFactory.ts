9a160d955dd2bed5447665300a795099d2ed58cb
MonikaZa <106819205+m0nikaza@users.noreply.github.com>
2026-02-20 14:05:48 +0200

    V6 user guides (#744)

--- generator/src/documentRoots/versioned/VersionedDocumentRootFactory.ts
+++ generator/src/documentRoots/versioned/VersionedDocumentRootFactory.ts
@@ -131,6 +131,10 @@ export class VersionedDocumentRootFactory implements IDocumentRootFactory {
       ...config.mdxFileProcessors
     ];
 
+    // if (!version.isLatest()) {
+    //   mdxFileProcessors.push(new ServerSideRenderProcessor());
+    // }
+
     if (this.appConfig.isDevMode()) {
       // Inject absolute file path for development purposes.
       mdxFileProcessors.push(new AbsolutePathProcessor(this.appConfig.getProjectRootDir()));
