bef330b7d6624fcb5dc6d782fb8f9f690428f0c1
Pavel Denisjuk <pavel@webiny.com>
2026-09-21 13:58:28 +0200

    chore: 5.44.2 changelog

--- docs/release-notes/navigation.tsx
+++ docs/release-notes/navigation.tsx
@@ -14,96 +14,96 @@ const HIDDEN_VERSIONS = ["6.5.0"];
 const cache = new Map<string, string[]>();
 
 const loadAllMdxFiles = (root: string) => {
-  if (!cache.has(root)) {
-    const files = globby.sync(`${root}**/*.mdx`).map(file => file.replace(rootDir, ""));
-    cache.set(root, files);
-  }
+    if (!cache.has(root)) {
+        const files = globby.sync(`${root}**/*.mdx`).map(file => file.replace(rootDir, ""));
+        cache.set(root, files);
+    }
 
-  return cache.get(root) as string[];
+    return cache.get(root) as string[];
 };
 
 enum ReleaseNotesType {
-  CHANGELOG = "changelog",
-  UPGRADE_GUIDE = "upgrade-guide"
+    CHANGELOG = "changelog",
+    UPGRADE_GUIDE = "upgrade-guide"
 }
 
 interface ReleaseNotes {
-  [version: string]: Array<{ type: ReleaseNotesType; file: string }>;
+    [version: string]: Array<{ type: ReleaseNotesType; file: string }>;
 }
 
 export const Navigation = () => {
-  const [allMdxFiles] = useState(() => loadAllMdxFiles(rootDir));
+    const [allMdxFiles] = useState(() => loadAllMdxFiles(rootDir));
 
-  const releases = useMemo(() => {
-    return allMdxFiles.reduce((acc, file) => {
-      const [version, type] = file.split("/") as [string, ReleaseNotesType];
-      if (valid(version)) {
-        return {
-          ...acc,
-          [version]: [...(acc[version] || []), { type, file }]
-        };
-      }
-      return acc;
-    }, {} as ReleaseNotes);
-  }, []);
+    const releases = useMemo(() => {
+        return allMdxFiles.reduce((acc, file) => {
+            const [version, type] = file.split("/") as [string, ReleaseNotesType];
+            if (valid(version)) {
+                return {
+                    ...acc,
+                    [version]: [...(acc[version] || []), { type, file }]
+                };
+            }
+            return acc;
+        }, {} as ReleaseNotes);
+    }, []);
 
-  const versions = useMemo(() => {
-    return Object.keys(releases)
-      .filter(version => !HIDDEN_VERSIONS.includes(version))
-      .sort((a, b) => {
-        return rcompare(a, b);
-      });
-  }, [releases]);
+    const versions = useMemo(() => {
+        return Object.keys(releases)
+            .filter(version => !HIDDEN_VERSIONS.includes(version))
+            .sort((a, b) => {
+                return rcompare(a, b);
+            });
+    }, [releases]);
 
-  const v6Versions = useMemo(() => versions.filter(v => !v.startsWith("5.")), [versions]);
-  const v5Versions = useMemo(() => versions.filter(v => v.startsWith("5.")), [versions]);
+    const v6Versions = useMemo(() => versions.filter(v => !v.startsWith("5.")), [versions]);
+    const v5Versions = useMemo(() => versions.filter(v => v.startsWith("5.")), [versions]);
 
-  const currentVersions = useMemo(() => v6Versions.slice(0, 5), [v6Versions]);
-  const olderVersions = useMemo(
-    () => [...v6Versions.slice(5), ...v5Versions],
-    [v6Versions, v5Versions]
-  );
+    const currentVersions = useMemo(() => v6Versions.slice(0, 5), [v6Versions]);
+    const olderVersions = useMemo(
+        () => [...v6Versions.slice(5), ...v5Versions],
+        [v6Versions, v5Versions]
+    );
 
-  function MenuItem({ version }: { version: string }) {
-    const predefinedTypes = [ReleaseNotesType.CHANGELOG, ReleaseNotesType.UPGRADE_GUIDE];
-    const changeLog = releases[version].find(item => item.type === ReleaseNotesType.CHANGELOG);
-    const upgrade = releases[version].find(item => item.type === ReleaseNotesType.UPGRADE_GUIDE);
-    const other = releases[version].filter(item => !predefinedTypes.includes(item.type));
+    function MenuItem({ version }: { version: string }) {
+        const predefinedTypes = [ReleaseNotesType.CHANGELOG, ReleaseNotesType.UPGRADE_GUIDE];
+        const changeLog = releases[version].find(item => item.type === ReleaseNotesType.CHANGELOG);
+        const upgrade = releases[version].find(
+            item => item.type === ReleaseNotesType.UPGRADE_GUIDE
+        );
+        const other = releases[version].filter(item => !predefinedTypes.includes(item.type));
+
+        return (
+            <Group title={version}>
+                {changeLog ? (
+                    <Page file={changeLog.file} link={`release-notes/${version}/changelog`} />
+                ) : null}
+                {upgrade ? (
+                    <Page file={upgrade.file} link={`release-notes/${version}/upgrade-guide`} />
+                ) : null}
+                {other.map(item => (
+                    <Page
+                        key={item.file}
+                        file={item.file}
+                        link={`release-notes/${item.file.replace(".mdx", "")}`}
+                    />
+                ))}
+            </Group>
+        );
+    }
 
     return (
-      <Group title={version}>
-        {changeLog ? (
-          <Page file={changeLog.file} link={`release-notes/${version}/changelog`} />
-        ) : null}
-        {upgrade ? (
-          <Page file={upgrade.file} link={`release-notes/${version}/upgrade-guide`} />
-        ) : null}
-        {other.map(item => (
-          <Page
-            key={item.file}
-            file={item.file}
-            link={`release-notes/${item.file.replace(".mdx", "")}`}
-          />
-        ))}
-      </Group>
+        <NavigationRoot directory={__dirname}>
+            <Group title={"Release Notes"}>
+                <Page file={"./upgrade-webiny.mdx"} link={"release-notes/upgrade-webiny"} />
+                {currentVersions.map(version => (
+                    <MenuItem key={version} version={version} />
+                ))}
+                <Group title={"Older Releases"}>
+                    {olderVersions.map(version => (
+                        <MenuItem key={version} version={version} />
+                    ))}
+                </Group>
+            </Group>
+        </NavigationRoot>
     );
-  }
-
-  return (
-    <NavigationRoot directory={__dirname}>
-      <Group title={"Release Notes"}>
-        <Page file={"./upgrade-webiny.mdx"} link={"release-notes/upgrade-webiny"} />
-        <Group title={"Release Notes"}>
-          {currentVersions.map(version => (
-            <MenuItem key={version} version={version} />
-          ))}
-          <Group title={"Older Releases"}>
-            {olderVersions.map(version => (
-              <MenuItem key={version} version={version} />
-            ))}
-          </Group>
-        </Group>
-      </Group>
-    </NavigationRoot>
-  );
 };
